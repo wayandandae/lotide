4555d59047b08419abc788f0c233fdd9b59c2adc
Jack <jkang0724@hotmail.com>
2022-11-15 01:29:51 +0000

    Custom console.assert messages

---
+++ assertEqual.js
@@ -0,0 +1,20 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  // if first and second parameters are strictly identical,
+  if (actual === expected) {
+    console.log("😎😎 Assertion Passed: " + actual + " === " + expected);
+    // if first and second parameters are unidentical,
+  } else {
+    console.log("🤯🤯 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+// unidentical strings
+assertEqual("Lighthouse Labs", "Bootcamp");
+// identical numbers
+assertEqual(1, 1);
+// identical strings
+assertEqual("Good", "Good");
+// unidentical numbers
+assertEqual(3, 5);
