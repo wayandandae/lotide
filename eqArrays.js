bca212a962b5e4bd5b2998f33e20212d356ac75a
Jack Kang <jkang0724@hotmail.com>
2022-11-15 21:43:59 -0500

    Added eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,30 @@
+// custom assert function to strictly compare two parameters
+const assertEqual = function(actual, expected) {
+  // if first and second parameters are strictly identical,
+  if (actual === expected) {
+    console.log(`😎😎 Assertion Passed: ${actual} === ${expected}`);
+    // if first and second parameters are unidentical,
+  } else {
+    console.log(`🤯🤯 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// compare two arrays by comparing each element of arrays
+const eqArrays = function(arrayOne, arrayTwo) {
+  // iterate through every element of arrayOne
+  for (let i = 0; i < arrayOne.length; i++) {
+    // if an element of arrayTwo at the same position is different,
+    if (arrayOne[i] !== arrayTwo[i]) return false;
+  }
+  // return boolean true if code inside for-loop is not triggered
+  return true;
+}
+
+// TEST CASES
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
