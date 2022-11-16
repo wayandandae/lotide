9e0d2d83012eeccb0a281e78ba4a5b34bae3a92e
Jack Kang <jkang0724@hotmail.com>
2022-11-15 22:02:33 -0500

    Removed redundant function assertEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,3 @@
-// custom assert function to strictly compare two parameters
-const assertEqual = function(actual, expected) {
-  // if first and second parameters are strictly identical,
-  if (actual === expected) {
-    console.log(`😎😎 Assertion Passed: ${actual} === ${expected}`);
-    // if first and second parameters are unidentical,
-  } else {
-    console.log(`🤯🤯 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 // compare two arrays by comparing each element of arrays
 const eqArrays = function(arrayOne, arrayTwo) {
   // iterate through every element of arrayOne
