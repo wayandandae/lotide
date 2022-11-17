88a8ed8c77eb9bd3333ca545c4200273dd4c4eac
Jack Kang <jkang0724@hotmail.com>
2022-11-17 15:50:49 -0500

    Function to map a new array using callback function

---
+++ map.js
@@ -0,0 +1,40 @@
+// compare two arrays by comparing each element of arrays
+const eqArrays = function(arrayOne, arrayTwo) {
+  // check if the lengths of two arrays are same
+  if (arrayOne.length !== arrayTwo.length) return false;
+  // iterate through every element of arrayOne
+  for (let i = 0; i < arrayOne.length; i++) {
+    // if an element of arrayTwo at the same position is different,
+    if (arrayOne[i] !== arrayTwo[i]) return false;
+  }
+  // return boolean true if code inside for-loop is not triggered
+  return true;
+};
+
+// compare two arrays and print out assertion message
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  // eqArrays either returns true or false
+  eqArrays(arrayOne, arrayTwo) ?
+    console.log(`😎 Assertion Passed: [${arrayOne}] is identical to [${arrayTwo}]`) : // true==pass message
+    console.log(`🤯 Assertion Failed: [${arrayOne}] is NOT identical to [${arrayTwo}]`); // false==fail message
+};
+
+// sample string data for map function
+const words = ["ground", "control", "to", "major", "tom"];
+
+// custom map function to create a new array based on callback applied on array
+const map = function(array, callback) {
+  // create a variable to hold elements
+  const results = [];
+  // iterate through every element of array
+  for (let item of array) {
+    // push every element satisfying callback function
+    results.push(callback(item));
+  }
+
+  return results;
+}
+
+// TEST CASE
+assertArraysEqual(map(words, word => word[0]), ["g", "c", "t", "m", "t"]);
+assertArraysEqual(map(words, word => word.length), [6, 7, 2, 5, 3]);
