1c0d0ad851fa6afa549dea882ee7a76f33d85c83
Jack Kang <jkang0724@hotmail.com>
2022-11-21 11:01:44 -0500

    Refactor and export eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,32 +1,15 @@
-// custom assert function to strictly compare two parameters
-const assertEqual = function(actual, expected) {
-  // if first and second parameters are strictly identical,
-  if (actual === expected) {
-    console.log(`😎😎 Assertion Passed: ${actual} === ${expected}`);
-    // if first and second parameters are unidentical,
-  } else {
-    console.log(`🤯🤯 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 // compare two arrays by comparing each element of arrays
-const eqArrays = function(arrayOne, arrayTwo) {
+const eqArrays = (arrayOne, arrayTwo) => {
   // check if the lengths of two arrays are same
   if (arrayOne.length !== arrayTwo.length) return false;
   // iterate through every element of arrayOne
   for (let i = 0; i < arrayOne.length; i++) {
-    // if an element of arrayTwo at the same position is different,
+    // if an element of arrayTwo at same index is different,
     if (arrayOne[i] !== arrayTwo[i]) return false;
   }
   // return boolean true if code inside for-loop is not triggered
   return true;
 };
 
-// TEST CASES
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+module.exports = eqArrays;
