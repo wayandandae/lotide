5bb34e7cb299bf454f716274957690907aaa10c7
Jack Kang <jkang0724@hotmail.com>
2022-11-21 10:49:49 -0500

    Refactor and export tail.js

--- tail.js
+++ tail.js
@@ -1,34 +1,5 @@
-// same function from assertEqual.js
-const assertEqual = function(actual, expected) {
-  // if first and second parameters are strictly identical,
-  if (actual === expected) {
-    console.log(`😎😎 Assertion Passed: ${actual} === ${expected}`);
-    // if first and second parameters are unidentical,
-  } else {
-    console.log(`🤯🤯 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// return every element of array but the first one
+const tail = array => array.slice(1);
 
-const tail = function(array) {
-  let newArray = [];
-  // array newArray contains every element but the first one of array
-  newArray = array.slice(1);
 
-  return newArray;
-};
-
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-// Test Case 2: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-// Test Case 3: Check empty array and array of single item
-const emptyArray = [];
-const singleArray = [1];
-// strictly comparing null values will always return false, using console.log instead
-console.log(tail(emptyArray));
-console.log(tail(singleArray));
+module.exports = tail;
